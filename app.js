d9989bff254b0d4a6a900656b2b3f3f0d54fdc07
Yash jangid <yash@ith.tech>
2025-03-14 20:02:40 +0530

    compare product update

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const path = require('path');
 
 const app = express();
 app.use(cors({
-  origin: ['http://localhost:5173','http://localhost:5174', 'http://localhost:3000'],
+  origin: ['http://localhost:5173','http://localhost:5174','https://select-ease-8fqv.vercel.app', 'http://localhost:3000'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
